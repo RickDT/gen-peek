b21f1ce24c5a4e16ad42aaf453422836d119dc15
Rick Terrill <rick.terrill@actualhq.com>
2026-03-31 12:39:47 -0500

    Expose type declarations in package exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
   dts: { oxc: true, resolver: "oxc" },
-  exports: true,
+  exports: false,
   clean: true,
   sourcemap: true,
 });
